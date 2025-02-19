db38a4e3e3ceef43934483d28bc12b2a8887ad17
mmskm1 <mmskm2015@gmail.com>
2025-02-19 03:02:30 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Tajawal', 'sans-serif'],
+      },
+      colors: {
+        primary: '#121212',
+        secondary: '#E6D5C9',
+      },
+      container: {
+        center: true,
+        padding: '1rem',
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1280px',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
